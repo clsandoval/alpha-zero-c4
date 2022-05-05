0b768688186fe686c0f88a51d641616bbcfe2014
unknown <clsandoval@up.edu.ph>
2022-05-06 03:33:39 +0800

    add coach

--- coach.py
+++ coach.py
@@ -1,2 +1,16 @@
 import numpy as np
 from mcts import MCTS
+
+class Coach():
+    
+    def __init__(self,nnet, game,num_sims, num_eps):
+        self.game = game
+        self.nnet=nnet 
+        self.mcts = MCTS(game,nnet,num_sims)
+        self.num_eps = num_eps
+
+    def episode():
+        pass
+
+    def train(self):
+        pass
